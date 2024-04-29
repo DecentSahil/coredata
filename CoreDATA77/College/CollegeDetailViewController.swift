d1cca05d13c4dfe99c28047ba0b825f46a2200be
DecentSahil <168282363+DecentSahil@users.noreply.github.com>
2024-04-29 15:28:54 +0530

    Update CollegeDetailViewController.swift

--- CoreDATA77/College/CollegeDetailViewController.swift
+++ CoreDATA77/College/CollegeDetailViewController.swift
@@ -35,7 +35,7 @@ class CollegeDetailViewController: UIViewController {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
+       
         // Pass the selected object to the new view controller.
     }
     */
