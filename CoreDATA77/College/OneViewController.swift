fd88fc693375b8c292abfeb06f92f7245b5bf3bc
Students <students@SAITMs-iMac-101.local>
2024-04-30 11:22:07 +0530

    add file

---
+++ CoreDATA77/College/OneViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OneViewController.swift
+//  CoreDATA77
+//
+//  Created by Students on 30/04/24.
+//
+
+import UIKit
+
+class OneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
